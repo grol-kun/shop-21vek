76ac46c8c8fd21f3128feea314860f447ed77c1c
Artem Pushnev <artem.pushnev@gmail.com>
2022-07-06 12:41:32 +0300

    feat: add homeblock to main-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { MainPageComponent } from './main-page/main-page.component';
+import { HomeblockComponent } from './main-page/homeblock/homeblock.component';
+import { PopularComponent } from './main-page/popular/popular.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    HomeblockComponent,
+    PopularComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/main-page/homeblock/homeblock.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homeblock',
+  templateUrl: './homeblock.component.html',
+  styleUrls: ['./homeblock.component.scss']
+})
+export class HomeblockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
