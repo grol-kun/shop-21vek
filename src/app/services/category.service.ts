f3a52ae209366407dba63e2bb6ee10e05a9093ee
Artem Pushnev <artem.pushnev@gmail.com>
2022-07-20 17:04:35 +0300

    feat: add category service

---
+++ src/app/services/category.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from "rxjs";
+import { baseUrl } from '../url';
+import { Category } from "../interfaces/category";
+
+@Injectable({providedIn: 'root'})
+export class CategoryServise {
+  categories:BehaviorSubject<Category[]>=new BehaviorSubject([] as Category[])
+  constructor(private httpClient: HttpClient) { }
+  fetchCategories(){
+       this.httpClient.get<Category[]>(baseUrl+'/categories').subscribe(data=>{
+            this.categories.next(data)
+       })
+  }
+
+}
