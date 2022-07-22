dc760224d2044f730028c09b771944e7ff05e4a0
Artem Pushnev <artem.pushnev@gmail.com>
2022-07-22 09:01:00 +0300

    feat: add footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  subscribed = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  subscribe() {
+    this.subscribed = true;
+  }
 }
